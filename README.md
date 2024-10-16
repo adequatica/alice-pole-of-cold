bf48b6d995601e951df2d9fc41da5167e891b9bf
Andrey Enin <enotramone@gmail.com>
2024-10-16 20:10:52 +0200

    Update comments and readme

--- README.md
+++ README.md
@@ -1,39 +1,19 @@
 # The Pole of Cold
 
-Serverless [skill for Alice](https://dialogs.yandex.ru/store/skills/8039a055-polyus-holod) ([virtual assistant](https://en.wikipedia.org/wiki/Alice_(virtual_assistant))) based on Node.js.
+Serverless [skill for Alice](https://dialogs.yandex.ru/store/skills/8039a055-polyus-holod) ([virtual assistant](<https://en.wikipedia.org/wiki/Alice_(virtual_assistant)>)) based on Node.js.
 
 It reports the weather (temperature) at the North and South Poles?
 
 ### How does it work?
 
-The user says: «*Alice, launch the skill The Pole of Cold*».
+The user says: «_Alice, launch the skill The Pole of Cold_».
 
-Alice responds with a welcome skill message: «*ask which pole is colder, north or south?*»
+Alice responds with a welcome skill message: «_Ask which pole is colder, north or south?_»
 
 On any following phrase of the user occurs:
-* Going to [OpenWeather API](https://openweathermap.org/api) follow the current weather at both poles;
-* Determines which pole is colder;
-* Answer to the user at which pole is colder and what is the temperature there.
+
+1. Going to [OpenWeather API](https://openweathermap.org/api) follow the current weather at both poles;
+2. Determines which pole is colder;
+3. Answer to the user at which pole is colder and what is the temperature there.
 
 The deployment occurs via the [Yandex Cloud Functions](https://cloud.yandex.com/en/services/functions).
-
-----
-
-# Полюс холода
-
-Бессерверный [навык Алисы](https://dialogs.yandex.ru/store/skills/8039a055-polyus-holod) на Node.js.
-
-Сообщает погоду (температуру) на Северном и Южном полюсах?
-
-### Как он работает?
-
-Пользователь говорит: «*Алиса, запусти навык полюс холода*»;
-
-Алиса отвечает приветственным сообщением навыка: «*cпросите какой полюс холоднее, северный или южный?*»;
-
-На любую следующую фразу пользователя происходит:
-* Поход в [OpenWeather API](https://openweathermap.org/api) за текущей погодой на обоих полюсах;
-* Определяется на каком полюсе холоднее;
-* Ответ пользователю на каком полюсе холоднее и какая там температура.
-
-Деплой происходит через [Функцию в Яндекс.Облаке](https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/).
--- index.js
+++ index.js
@@ -2,48 +2,46 @@
 // https://nodejs.org/api/https.html
 const https = require("https");
 
-// Weather API / Погодное API
+// Weather API
 // https://openweathermap.org/current
 const apiWeater = "https://api.openweathermap.org/data/2.5/weather";
 // Secret key is stored in environment variable
-// Секретик ключа лежит в переменной окружения
 // https://nodejs.org/api/process.html#process_process_env
 const apiWeatherKey = process.env.key;
 
 // For a serverless application, you need to write API calls on a «pure node» (probably)
-// Для serverless приложения нужно написать вызовы к API на «чистой ноде» (наверное)
 // https://cloud.yandex.com/en/docs/functions/concepts/function
-const getHttps = (url) => new Promise((resolve, reject) => {
-    https.get(url, res => {
-        if (res.statusCode === 200) {
-            let rawData = "";
-            res.on("data", chunk => {
-                rawData += chunk;
-            });
-            res.on("end", () => {
-                try {
-                    const response = JSON.parse(rawData);
-                    resolve(response);
-                } catch (err) {
-                    reject(err.message);
-                }
-            }).on("error", err => {
-                console.log(`Error: ${err.message}`);
-            });
-        } else {
-            console.log("Error");
-        }
+const getHttps = (url) =>
+  new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 200) {
+        let rawData = "";
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+        res
+          .on("end", () => {
+            try {
+              const response = JSON.parse(rawData);
+              resolve(response);
+            } catch (err) {
+              reject(err.message);
+            }
+          })
+          .on("error", (err) => {
+            console.log(`Error: ${err.message}`);
+          });
+      } else {
+        console.log("Error");
+      }
     });
-});
+  });
 
 // Creating skills for Alice
-// Создание навыка в Яндекс.Облаке
 // https://cloud.yandex.com/en/docs/functions/tutorials/alice-skill
 // Creating a serverless function in Yandex.Cloud
-// Создание функции в Яндекс.Облаке
 // https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/#create-function
 // Example of a skill using a serverless function
-// Пример навыка с использованием serverless функции
 // https://github.com/yandex-cloud/examples/blob/master/serverless/functions/alice/nodejs/parrot/index.js
 /**
  * Entry-point for Serverless Function.
@@ -53,96 +51,95 @@ const getHttps = (url) => new Promise((resolve, reject) => {
  * @return {Promise<Object>} response to be serialized as JSON.
  */
 module.exports.handler = async (event) => {
-    // https://yandex.ru/dev/dialogs/alice/doc/protocol-docpage/
-    const {request, version, session} = event;
+  // https://yandex.ru/dev/dialogs/alice/doc/protocol-docpage/
+  const { request, version, session } = event;
 
-    // Skill MUST contain welcome message and help
-    // Навык ДОЛЖЕН содержать приветственное сообщение и help
-    // https://yandex.ru/dev/dialogs/alice/doc/requirements-docpage/#specific__content
-    // "Ask which pole is colder, north or south?"
-    const textWelcome = "Спросите какой полюс холоднее, северный или южный?";
-    // "I will answer any question with the weather at the poles"
-    const textHelp = "На любой вопрос отвечу погодой на полюсах";
+  // Skill MUST contain welcome message and help
+  // https://yandex.ru/dev/dialogs/alice/doc/requirements-docpage/#specific__content
+  // "Ask which pole is colder, north or south?"
+  const textWelcome = "Спросите какой полюс холоднее, северный или южный?";
+  // "I will answer any question with the weather at the poles"
+  const textHelp = "На любой вопрос отвечу погодой на полюсах";
 
-    if (session.new) {
-        return {
-            version,
-            session,
-            response: {
-                text: textWelcome,
-                end_session: false
-            }
-        };
-    }
+  if (session.new) {
+    return {
+      version,
+      session,
+      response: {
+        text: textWelcome,
+        end_session: false,
+      },
+    };
+  }
 
-    const command = request.command.toLowerCase();
+  const command = request.command.toLowerCase();
 
-    // "help" || "what can you do"
-    if (command.includes("помощь") || command.includes("что ты умеешь")) {
-        return {
-            version,
-            session,
-            response: {
-                text: textHelp,
-                end_session: false
-            }
-        };
-    }
+  // "help" || "what can you do"
+  if (command.includes("помощь") || command.includes("что ты умеешь")) {
+    return {
+      version,
+      session,
+      response: {
+        text: textHelp,
+        end_session: false,
+      },
+    };
+  }
 
-    const weatherNorth = await getHttps(`${apiWeater}?lat=90.00000&lon=0.00000&units=metric&appid=${apiWeatherKey}`);
-    const weatherSouth = await getHttps(`${apiWeater}?lat=-90.00000&lon=0.00000&units=metric&appid=${apiWeatherKey}`);
+  const weatherNorth = await getHttps(
+    `${apiWeater}?lat=90.00000&lon=0.00000&units=metric&appid=${apiWeatherKey}`
+  );
+  const weatherSouth = await getHttps(
+    `${apiWeater}?lat=-90.00000&lon=0.00000&units=metric&appid=${apiWeatherKey}`
+  );
 
-    let tempNorth = Math.round(weatherNorth.main.temp);
-    let tempSouth = Math.round(weatherSouth.main.temp);
+  let tempNorth = Math.round(weatherNorth.main.temp);
+  let tempSouth = Math.round(weatherSouth.main.temp);
 
-    let answer;
+  let answer;
 
-    // Easter egg ("warmer")
-    // Пасхалка
-    if (command.includes("теплее")) {
-
-        if (tempNorth > tempSouth) {
-            // `It is warmer at the North Pole now, there is ${tempNorth}. At the South Pole is ${tempSouth}`
-            answer = `Сейчас теплее на Северном полюсе, там ${tempNorth}. На Южном полюсе ${tempSouth}`;
-        } else if (tempNorth < tempSouth) {
-            // `It is warmer at the South Pole now, there is ${tempNorth}. At the North Pole is ${tempSouth}`
-            answer = `Сейчас теплее на Южном полюсе, там ${tempSouth}. На Северном полюсе ${tempNorth}`;
-        } else {
-            // `It is equally ${tempNorth} at both poles`
-            answer = `На обоих полюсах одинаково ${tempNorth}`;
-        }
-
-        return {
-            version,
-            session,
-            response: {
-                text: answer,
-                // Finish dialog after answer
-                // Завершаем разговор после ответа
-                end_session: true
-            }
-        };
-    }
-
+  // Easter egg ("warmer")
+  if (command.includes("теплее")) {
     if (tempNorth > tempSouth) {
-        // `It is colder at the South Pole now, there is ${tempNorth}. At the North Pole is ${tempSouth}`
-        answer = `Сейчас холоднее на Южном полюсе, там ${tempSouth}. На Северном полюсе ${tempNorth}`;
+      // `It is warmer at the North Pole now, there is ${tempNorth}. At the South Pole is ${tempSouth}`
+      answer = `Сейчас теплее на Северном полюсе, там ${tempNorth}. На Южном полюсе ${tempSouth}`;
     } else if (tempNorth < tempSouth) {
-        // `It is colder at the North Pole now, there is ${tempNorth}. At the South Pole is ${tempSouth}`
-        answer = `Сейчас холоднее на Северном полюсе, там ${tempNorth}. На Южном полюсе ${tempSouth}`;
+      // `It is warmer at the South Pole now, there is ${tempNorth}. At the North Pole is ${tempSouth}`
+      answer = `Сейчас теплее на Южном полюсе, там ${tempSouth}. На Северном полюсе ${tempNorth}`;
     } else {
-        // `It is equally ${tempNorth} at both poles`
-        answer = `На обоих полюсах одинаково ${tempNorth}`;
+      // `It is equally ${tempNorth} at both poles`
+      answer = `На обоих полюсах одинаково ${tempNorth}`;
     }
 
     return {
-        version,
-        session,
-        response: {
-            text: answer,
-            // Finish dialog after answer
-            // Завершаем разговор после ответа
-            end_session: true
-        }
+      version,
+      session,
+      response: {
+        text: answer,
+        // Finish dialog after answer
+        end_session: true,
+      },
     };
+  }
+
+  if (tempNorth > tempSouth) {
+    // `It is colder at the South Pole now, there is ${tempNorth}. At the North Pole is ${tempSouth}`
+    answer = `Сейчас холоднее на Южном полюсе, там ${tempSouth}. На Северном полюсе ${tempNorth}`;
+  } else if (tempNorth < tempSouth) {
+    // `It is colder at the North Pole now, there is ${tempNorth}. At the South Pole is ${tempSouth}`
+    answer = `Сейчас холоднее на Северном полюсе, там ${tempNorth}. На Южном полюсе ${tempSouth}`;
+  } else {
+    // `It is equally ${tempNorth} at both poles`
+    answer = `На обоих полюсах одинаково ${tempNorth}`;
+  }
+
+  return {
+    version,
+    session,
+    response: {
+      text: answer,
+      // Finish dialog after answer
+      end_session: true,
+    },
+  };
 };
